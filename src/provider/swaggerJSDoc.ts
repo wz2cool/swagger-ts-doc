ba654fdca57edddbef5b791f5d01e97ea80cf3dc
wz2cool <wz2cool@live.cn>
2018-04-13 11:04:29 +0800

    add

--- src/provider/swaggerJSDoc.ts
+++ src/provider/swaggerJSDoc.ts
@@ -9,7 +9,7 @@ export function swaggerJSDoc(options: SwaggerOptions): string {
     const paths = SwaggerHelper.generatePaths(requestMappingCache.getRequestMappingInfos());
 
     const result: any = {};
-    result.info = options;
+    result.info = options.info;
     result.schemes = ["http", "https"];
     result.definitions = definitions;
     result.paths = paths;
