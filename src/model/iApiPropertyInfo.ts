cb70218d77b7d7a65ccf86fe45878a9e73ab8d6c
wz2cool@live.cn <wz2cool@live.cn>
2018-12-16 23:32:24 +0800

    add

--- src/model/iApiPropertyInfo.ts
+++ src/model/iApiPropertyInfo.ts
@@ -1,18 +1,24 @@
 import { DataType } from ".";
 
 export interface IApiPropertyInfo {
+  /**
+   * @summary the type of property.
+   */
   dataType: DataType;
   /**
-   * @summary the
+   * @summary whether property is required.
+   * @default false
    */
   required?: boolean;
   /**
    * @summary the description of property.
+   * @default ""
    */
   description?: string;
   /**
    * @summary reference a definition.
    * @tutorial https://swagger.io/docs/specification/using-ref/
+   * @default undefined
    */
   refModel?: { new (): any } | DataType;
 }
